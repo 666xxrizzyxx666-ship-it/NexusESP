c9f5833771b8a8e9888d74114fc8ae984780f3ac
rizzy <666xxrizzyxx666@gmail.com>
2026-03-03 19:47:37 +0100

    Update ESP.lua

--- Modules/ESP.lua
+++ Modules/ESP.lua
@@ -1,9 +1,22 @@
 local ESP = {}
 
 ESP.Enabled = false
+ESP.Boxes = {}
 
 function ESP:UpdateAll()
-    print("ESP UpdateAll() OK")
+    if not self.Enabled then return end
+
+    for _, player in ipairs(game.Players:GetPlayers()) do
+        if player ~= game.Players.LocalPlayer then
+
+            if not self.Boxes[player] then
+                getgenv().Box.Create(player)
+                self.Boxes[player] = true
+            end
+
+            getgenv().Box.Update(player)
+        end
+    end
 end
 
 return ESP
