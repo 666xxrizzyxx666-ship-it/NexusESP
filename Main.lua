3130c67106880a106e8d89639c6a3dcfc8b7fefc
rizzy <666xxrizzyxx666@gmail.com>
2026-03-03 17:48:49 +0100

    Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,35 @@
+-- Main.lua
 
+local function include(url)
+    return loadstring(game:HttpGet(url))()
+end
+
+local base = "https://raw.githubusercontent.com/TON-PSEUDO/NexusESP/main/Modules/"
+
+-- ESP MASTER TABLE
+ESP = {
+    Enabled = true,
+
+    Boxes = {},
+    HealthBars = {},
+    Skeletons = {},
+    Names = {},
+    Tracers = {},
+    Glows = {},
+
+    Box = false,
+    BoxColor = Color3.fromRGB(255, 0, 0),
+}
+
+include(base .. "Box.lua")
+
+game:GetService("RunService").RenderStepped:Connect(function()
+    for _, player in pairs(game:GetService("Players"):GetPlayers()) do
+        if player ~= game.Players.LocalPlayer then
+            local char = player.Character
+            if char then
+                UpdateBox(player, char)
+            end
+        end
+    end
+end)
