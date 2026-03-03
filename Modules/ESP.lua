109a536b7760529346b1295a9e9706bea390e3f5
rizzy <666xxrizzyxx666@gmail.com>
2026-03-03 19:20:03 +0100

    Update ESP.lua

--- Modules/ESP.lua
+++ Modules/ESP.lua
@@ -16,24 +16,22 @@ function ESP:UpdateAll()
     for _, player in ipairs(Players:GetPlayers()) do
         if player ~= LocalPlayer then
 
-            -- BOX
             if self.Enabled then
                 if not self.Boxes[player] then
-                    require(script.Parent.Box).Create(player)
+                    getgenv().Box.Create(player)
                 end
-                require(script.Parent.Box).Update(player)
+                getgenv().Box.Update(player)
             else
                 if self.Boxes[player] then
                     self.Boxes[player].Visible = false
                 end
             end
 
-            -- HEALTH
             if self.HealthBar then
                 if not self.HealthBars[player] then
-                    require(script.Parent.Health).Create(player)
+                    getgenv().Health.Create(player)
                 end
-                require(script.Parent.Health).Update(player)
+                getgenv().Health.Update(player)
             else
                 local hb = self.HealthBars[player]
                 if hb then
