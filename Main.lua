900c499fbf512e42593a4f8db3daf25fb5289f76
rizzy <666xxrizzyxx666@gmail.com>
2026-03-03 20:17:31 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,22 +1,17 @@
---==============================--
---  TEST 7 : BOX ESP DRAWING
---==============================--
-
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/Library.lua"))()
 
 local Window = Library:CreateWindow({
-    Title = "Nexus by Rizzy - TEST 7",
+    Title = "Nexus by Rizzy - CLEAN TEST",
     Center = true,
     AutoShow = true
 })
 
 local Tab = Window:AddTab("Main")
-local Group = Tab:AddLeftGroupbox("ESP Test")
+local Group = Tab:AddLeftGroupbox("ESP")
 
 local function include(path)
     local url = "https://raw.githubusercontent.com/666xxrizzyxx666-ship-it/NexusESP/main/" .. path
-    local content = game:HttpGet(url)
-    return loadstring(content)()
+    return loadstring(game:HttpGet(url))()
 end
 
 local ESP = include("Modules/ESP.lua")
